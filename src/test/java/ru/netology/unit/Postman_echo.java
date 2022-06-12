e313b05c4f4d6370bba453a6a976c8b0fb6f790b
JMatweewa <93136237+JMatweewa@users.noreply.github.com>
2022-06-12 19:38:38 +0700

    test crashes

---
+++ src/test/java/ru/netology/unit/Postman_echo.java
@@ -0,0 +1,24 @@
+package ru.netology.unit;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class Postman_echo {
+
+    @Test
+    void shouldReturnPost() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+
+                .when()
+                .post("/post")
+
+                .then()
+
+                .statusCode(200)
+                .body("data", equalTo("average data value"));
+    }
+}
